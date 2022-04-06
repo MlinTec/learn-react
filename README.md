e08a2c272753251cc7b83bb92c0c29ed7b8c78cd
MlinTec <ml28888@126.com>
2022-04-06 13:48:48 +0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# react-igkwtj
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/react-igkwtj)
---
+++ src/components/Post.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export default function Post() {
+  return (
+    <div class="post">
+      <h2 class="post__title">Title</h2>
+      <p class="post__text">Post main context!</p>
+    </div>
+  );
+}
